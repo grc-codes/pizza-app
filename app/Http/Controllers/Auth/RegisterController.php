d926ea9104f0ed17b545e466f8e50e65ae3b1a6b
grc-codes <grc.codes@gmail.com>
2020-04-14 19:31:22 -0400

    created a method to edit and delete users

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
             'fname' => ['required', 'string', 'max:255'],
             'lname' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
         ]);
     }
 
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'fname' => $data['fname'],
             'lname' => $data['lname'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => Hash::make($data['password'])
         ]);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class UsersTableSeeder extends Seeder
 {
@@ -15,14 +16,18 @@ class UsersTableSeeder extends Seeder
             'fname' => 'Adam',
             'lname' => 'Smith',
             'email' => 'asmith@gmail.com',
-            'password' => Hash::make('rootroot')
+            'password' => Hash::make('rootroot'),
+            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()
         ]);
 
         DB::table('users')->insert([
             'fname' => 'Cindy',
             'lname' => 'Johnson',
             'email' => 'cjohnson@gmail.com',
-            'password' => Hash::make('rootroot')
+            'password' => Hash::make('rootroot'),
+            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()
         ]);
     }
 }
