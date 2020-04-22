981c1627e5097e4d073b8095cb2995d2d0fa5c1a
grc-codes <grc.codes@gmail.com>
2020-04-21 21:41:05 -0400

    created new members from offers page

---
+++ resources/views/pages/thank-you.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="offers-page">
+        <div class="content-box">
+            <div class="row">
+                <div class="col-md-8 offset-md-2 thank-you">
+                    <h1>Thank you!</h1>
+                    <p>You will be notified of all special offers we have through email.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
